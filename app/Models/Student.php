e243284632670aa4517cf9bdc09e15e4f728f749
Reew52 <112863268+Reew52@users.noreply.github.com>
2023-06-15 10:42:05 +0700

    update list student api

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -1,11 +1,12 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
 class Student extends Model
 {
+    protected $primaryKey = 'student_id';
     protected $fillable = [
         'student_id',
         'major_id',
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\API\StudentController;
+use App\Http\Controllers\API\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,6 +15,24 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+// Users
+Route::group(['prefix' => 'users'], function () {
+    Route::get('/', [UserController::class, 'index']);
+    Route::post('/add-user', [UserController::class, 'store']);
+    Route::get('/edit-user/{id}', [UserController::class, 'edit']);
+    Route::put('/update-user/{id}', [UserController::class, 'update']);
+    Route::delete('/delete-user/{id}', [UserController::class, 'delete']);
+});
+
+// students
+Route::group(['prefix' => 'students'], function () {
+    Route::get('/', [StudentController::class, 'index']);
+    Route::post('/add-student', [StudentController::class, 'store']);
+    Route::get('/edit-student/{id}', [StudentController::class, 'edit']);
+    Route::put('/update-student/{id}', [StudentController::class, 'update']);
+    Route::delete('/delete-student/{id}', [StudentController::class, 'delete']);
+});
+
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
