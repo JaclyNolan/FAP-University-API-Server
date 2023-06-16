f18acf1b175d458f530320e1b54a3aa07b33bd47
Reew52 <112863268+Reew52@users.noreply.github.com>
2023-06-16 16:12:50 +0700

    CRUD-Student

--- app/Http/Controllers/API/StudentController.php
+++ app/Http/Controllers/API/StudentController.php
@@ -11,49 +11,85 @@ use Illuminate\Http\Request;
 class StudentController extends Controller
 {
     //
-    public function index()
-    {
-        $student = new Student;
+    private $student;
 
-        $students = Student::select(
-            $student->getTable() . '.student_id AS id',
-            $student->getTable() . '.full_name',
-            $student->getTable() . '.image',
-            $student->getTable() . '.gender',
-            $student->getTable() . '.academic_year',
-            $student->getTable() . '.date_of_birth AS Dob',
+    public function __construct()
+    {
+        $this->student = new Student;
+    }
+    public function index(Request $request)
+    {
+        $query = Student::select(
+            $this->student->getTable() . '.student_id AS id',
+            $this->student->getTable() . '.full_name',
+            $this->student->getTable() . '.image',
+            $this->student->getTable() . '.gender',
+            $this->student->getTable() . '.academic_year',
+            $this->student->getTable() . '.date_of_birth AS Dob',
             (new User)->getTable() . '.email',
-            $student->getTable() . '.phone_number AS phone',
-            $student->getTable() . '.address',
+            $this->student->getTable() . '.phone_number AS phone',
+            $this->student->getTable() . '.address',
             (new Major)->getTable() . '.major_name'
         )
-            ->leftJoin((new User)->getTable(), $student->getTable() . '.student_id', '=', (new User)->getTable() . '.student_id')
-            ->join((new Major)->getTable(), $student->getTable() . '.major_id', '=', (new Major)->getTable() . '.major_id')
-            ->orderBy($student->getTable() . '.student_id')
-            ->get();
+            ->leftJoin((new User)->getTable(), $this->student->getTable() . '.student_id', '=', (new User)->getTable() . '.student_id')
+            ->join((new Major)->getTable(), $this->student->getTable() . '.major_id', '=', (new Major)->getTable() . '.major_id')
+            ->whereNull($this->student->getTable() . '.deleted_at')
+            ->orderBy($this->student->getTable() . '.student_id');
+
+        // Áp dụng bộ lọc nếu có
+        if ($request->has('gender')) {
+            $gender = $request->input('gender');
+            $query->where($this->student->getTable() . '.gender', $gender);
+        }
+
+        if ($request->has('academic_year')) {
+            $academicYear = $request->input('academic_year');
+            $query->where($this->student->getTable() . '.academic_year', $academicYear);
+        }
+
+        if ($request->has('status')) {
+            $status = $request->input('status');
+            if ($status == 'active') {
+                $query->whereNull((new User)->getTable() . '.deleted_at');
+            } elseif ($status == 'inactive') {
+                $query->whereNotNull((new User)->getTable() . '.deleted_at');
+            }
+        }
+
+        // Áp dụng tìm kiếm từ khóa
+        if ($request->has('keyword')) {
+            $keyword = $request->input('keyword');
+            $query->where(function ($q) use ($keyword) {
+                $q->where($this->student->getTable() . '.full_name', 'LIKE', "%$keyword%")
+                    ->orWhere((new User)->getTable() . '.email', 'LIKE', "%$keyword%")
+                    ->orWhere($this->student->getTable() . '.phone_number', 'LIKE', "%$keyword%");
+            });
+        }
+
+        $students = $query->paginate(10); // Số bản ghi trên mỗi trang
 
         return response()->json([
             'status' => 200,
-            'students' => $students,
+            'students' => $students->items(),
+            'total_pages' => $students->lastPage(),
         ]);
     }
 
     public function store(Request $request)
     {
-        $student = new Student;
-        $student->student_id = $request->input('student_id');
-        $student->full_name = $request->input('full_name');
-        $student->gender = $request->input('gender');
-        $student->major_id = $request->input('major_id');
-        $student->address = $request->input('address');
-        $student->phone_number = $request->input('phone_number');
-        $student->status = $request->input('status');
-        $student->image = $request->input('image');
-        $student->date_of_birth = $request->input('date_of_birth');
-        $student->academic_year = $request->input('academic_year');
+        $this->student->student_id = $request->input('student_id');
+        $this->student->full_name = $request->input('full_name');
+        $this->student->gender = $request->input('gender');
+        $this->student->major_id = $request->input('major_id');
+        $this->student->address = $request->input('address');
+        $this->student->phone_number = $request->input('phone_number');
+        $this->student->status = $request->input('status');
+        $this->student->image = $request->input('image');
+        $this->student->date_of_birth = $request->input('date_of_birth');
+        $this->student->academic_year = $request->input('academic_year');
 
-        $student->created_at = date('Y-m-d H:i:s'); // Lấy thời gian hiện tại
-        $student->save();
+        $this->student->created_at = date('Y-m-d H:i:s'); // Lấy thời gian hiện tại
+        $this->student->save();
 
         return response()->json([
             'status' => 200,
@@ -63,11 +99,61 @@ class StudentController extends Controller
 
     public function edit($id)
     {
-        $student = Student::find($id);
+        $student = Student::select(
+            $this->student->getTable() . '.student_id AS id',
+            $this->student->getTable() . '.full_name',
+            $this->student->getTable() . '.image',
+            $this->student->getTable() . '.gender',
+            $this->student->getTable() . '.academic_year',
+            $this->student->getTable() . '.date_of_birth AS Dob',
+            (new User)->getTable() . '.email',
+            $this->student->getTable() . '.phone_number AS phone',
+            $this->student->getTable() . '.address',
+            $this->student->getTable() . '.gpa',
+            $this->student->getTable() . '.status',
+            (new Major)->getTable() . '.major_name'
+        )
+            ->leftJoin((new User)->getTable(),  $this->student->getTable() . '.student_id', '=', (new User)->getTable() . '.student_id')
+            ->join((new Major)->getTable(),  $this->student->getTable() . '.major_id', '=', (new Major)->getTable() . '.major_id')
+            ->where($this->student->getTable() . '.student_id', $id)
+            ->first();
 
         return response()->json([
             'status' => 200,
             'student' => $student,
+        ]);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $student = $this->student::find($id);
+        $student->full_name = $request->input('full_name');
+        $student->date_of_birth = $request->input('date_of_birth');
+        $student->gender = $request->input('gender');
+        $student->address = $request->input('address');
+        $student->phone_number = $request->input('phone_number');
+        $student->status = $request->input('status');
+        $student->major_id = $request->input('major_id');
+        $student->academic_year = $request->input('academic_year');
+        $student->image = $request->input('image');
+        $student->updated_at = date('Y-m-d H:i:s'); // Lấy thời gian hiện tại
+        $student->update();
+
+        return response()->json([
+            'status' => 200,
+            'message' => 'Student Update Successfully!',
+        ]);
+    }
+
+    public function delete($id)
+    {
+        $student = $this->student::find($id);
+        $student->deleted_at = date('Y-m-d H:i:s');
+        $student->update();
+
+        return response()->json([
+            'status' => 200,
+            'message' => 'Student Delete Successfully!',
         ]);
     }
 }
