8d5e552307b85ba7f7e81dea533c413e8fc192e3
JaclyNolan <anhbg330011@gmail.com>
2023-06-19 19:26:48 +0700

    Add middleware role checking
    - Added 'isAdmin' Gate
    - Added 'isAdminOrStaff' Gate
    - Added 'isInstructor' Gate
    - Added 'isStudent' Gate

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
 use App\Http\Controllers\API\Admin\UserController;
-use App\Http\Controllers\API\Admin\AuthController;
+use App\Http\Controllers\API\AuthController;
 use App\Http\Controllers\API\Admin\StudentController;
 use App\Http\Controllers\API\Admin\StaffController;
 use App\Http\Controllers\API\Admin\InstructorController;
@@ -20,46 +20,61 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
-// Users
-Route::group(['prefix' => 'users'], function () {
-    Route::get('/', [UserController::class, 'index']);
-    Route::post('/add-user', [UserController::class, 'store']);
-    Route::get('/edit-user/{id}', [UserController::class, 'edit']);
-    Route::put('/update-user/{id}', [UserController::class, 'update']);
-    Route::put('/delete-user/{id}', [UserController::class, 'delete']);
-});
-// students
-Route::group(['prefix' => 'students'], function () {
-    Route::get('/', [StudentController::class, 'index']);
-    Route::post('/add-student', [StudentController::class, 'store']);
-    Route::get('/edit-student/{id}', [StudentController::class, 'edit']);
-    Route::put('/update-student/{id}', [StudentController::class, 'update']);
-    Route::put('/delete-student/{id}', [StudentController::class, 'delete']);
-});
-// staffs
-Route::group(['prefix' => 'staffs'], function () {
-    Route::get('/', [StaffController::class, 'index']);
-    Route::post('/add-staff', [StaffController::class, 'store']);
-    Route::get('/edit-staff/{id}', [StaffController::class, 'edit']);
-    Route::put('/update-staff/{id}', [StaffController::class, 'update']);
-    Route::put('/delete-staff/{id}', [StaffController::class, 'delete']);
-});
-// instructors
-Route::group(['prefix' => 'instructors'], function () {
-    Route::get('/', [InstructorController::class, 'index']);
-    Route::post('/add-instructor', [InstructorController::class, 'store']);
-    Route::get('/edit-instructor/{id}', [InstructorController::class, 'edit']);
-    Route::put('/update-instructor/{id}', [InstructorController::class, 'update']);
-    Route::put('/delete-instructor/{id}', [InstructorController::class, 'delete']);
+
+Route::middleware(['auth:sanctum', 'can:isAdmin'])->group(function () {
+    // User with admin  role can access these routes
+    Route::group(['prefix' => 'users'], function () {
+        Route::get('/', [UserController::class, 'index']);
+        Route::post('/add-user', [UserController::class, 'store']);
+        Route::get('/edit-user/{id}', [UserController::class, 'edit']);
+        Route::put('/update-user/{id}', [UserController::class, 'update']);
+        Route::put('/delete-user/{id}', [UserController::class, 'delete']);
+    });
 });
 
-// newContents
-Route::group(['prefix' => 'newContents'], function () {
-    Route::get('/', [NewsController::class, 'index']);
-    Route::post('/add-newContent', [NewsController::class, 'store']);
-    Route::get('/edit-newContent/{id}', [NewsController::class, 'edit']);
-    Route::put('/update-newContent/{id}', [NewsController::class, 'update']);
-    Route::put('/delete-newContent/{id}', [NewsController::class, 'delete']);
+Route::middleware(['auth:sanctum', 'can:isAdminOrStaff'])->group(function () {
+    // User with admin and staff role can access these routes
+    // students
+    Route::group(['prefix' => 'students'], function () {
+        Route::get('/', [StudentController::class, 'index']);
+        Route::post('/add-student', [StudentController::class, 'store']);
+        Route::get('/edit-student/{id}', [StudentController::class, 'edit']);
+        Route::put('/update-student/{id}', [StudentController::class, 'update']);
+        Route::put('/delete-student/{id}', [StudentController::class, 'delete']);
+    });
+    // staffs
+    Route::group(['prefix' => 'staffs'], function () {
+        Route::get('/', [StaffController::class, 'index']);
+        Route::post('/add-staff', [StaffController::class, 'store']);
+        Route::get('/edit-staff/{id}', [StaffController::class, 'edit']);
+        Route::put('/update-staff/{id}', [StaffController::class, 'update']);
+        Route::put('/delete-staff/{id}', [StaffController::class, 'delete']);
+    });
+    // instructors
+    Route::group(['prefix' => 'instructors'], function () {
+        Route::get('/', [InstructorController::class, 'index']);
+        Route::post('/add-instructor', [InstructorController::class, 'store']);
+        Route::get('/edit-instructor/{id}', [InstructorController::class, 'edit']);
+        Route::put('/update-instructor/{id}', [InstructorController::class, 'update']);
+        Route::put('/delete-instructor/{id}', [InstructorController::class, 'delete']);
+    });
+
+    // newContents
+    Route::group(['prefix' => 'newContents'], function () {
+        Route::get('/', [NewsController::class, 'index']);
+        Route::post('/add-newContent', [NewsController::class, 'store']);
+        Route::get('/edit-newContent/{id}', [NewsController::class, 'edit']);
+        Route::put('/update-newContent/{id}', [NewsController::class, 'update']);
+        Route::put('/delete-newContent/{id}', [NewsController::class, 'delete']);
+    });
+});
+
+Route::middleware(['auth:sanctum', 'can:isInstructor'])->group(function() {
+    // User with instructor role can access these routes
+});
+
+Route::middleware(['auth:sanctum', 'can:isStudent'])->group(function() {
+    // User with student role can access these routes
 });
 
 Route::middleware('auth:sanctum')->group(function () {
